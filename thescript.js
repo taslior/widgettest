e84648ac390d1bf6535799bf6b0e3b84e726033b
taslior <61362158+taslior@users.noreply.github.com>
2021-05-21 09:25:25 +0200

    Update thescript.js

--- thescript.js
+++ thescript.js
@@ -2,6 +2,19 @@ async function createWidget() {
   // Create new empty ListWidget instance
   let listwidget = new ListWidget();
 
+    // Set new background color
+  listwidget.backgroundColor = new Color("#000000");
+
+  // Add widget heading
+  let heading = listwidget.addText("🚀Next🚀");
+  heading.centerAlignText();
+  heading.font = Font.lightSystemFont(25);
+  heading.textColor = new Color("#ffffff");
+
+  // Spacer between heading and launch date
+  listwidget.addSpacer(15);
+  
+  
   // Return the created widget
   return listwidget;
 }
