7915f3ae8cb700f1fbf76e9a760dda698deb5fa7
taslior <61362158+taslior@users.noreply.github.com>
2021-05-21 10:00:57 +0200

    Update thescript.js

--- thescript.js
+++ thescript.js
@@ -56,6 +56,10 @@ function displayLaunchDateTime(stack, launchDateTime, precision) {
   }
 }
 
+async function getData() {
+    // Query URL
+  const url = "https://services7.arcgis.com/0Uc5jDlEgdLosloE/ArcGIS/rest/services/Gemeinden/FeatureServer/0/query?where=gmdnr+%3D+%278226054%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=af%2C+af_dif&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
+  }
 
 async function getNextLaunch() {
   // Query url
